1f89ab0813015fe1f78ba61796289bdef6bfc0bd
Никита <immortallatrommi22@gmail.com>
2024-04-13 01:20:20 +0700

    fix: change description of routes

--- routes.ts
+++ routes.ts
@@ -2,7 +2,6 @@
 export const publicRoutes = ["/"]
 
 // An array of routes that are used for authentication
-// These routes will redirect logged in user to home page
 export const authRoutes = [
   "/signin",
   "/signup",
